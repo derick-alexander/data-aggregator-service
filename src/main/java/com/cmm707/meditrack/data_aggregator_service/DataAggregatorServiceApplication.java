2bdad204e068ec0fb0ceb674ab354be9fe12ec4e
derick-alexander <derickawsalexander@gmail.com>
2024-12-17 22:11:57 +0530

    initial commit

---
+++ src/main/java/com/cmm707/meditrack/data_aggregator_service/DataAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.data_aggregator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmm707/meditrack/data_aggregator_service/DataAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.data_aggregator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
